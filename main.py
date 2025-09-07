7812a7ec7d2fb050cc97923cca959991ed19df8b
supermeisi <mustafa_schmidt@gmx.de>
2025-09-07 11:04:38 +0200

    Adding initial conditions

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import cylinder
 
 def main():
     r = ray.Ray()
+    
+    # Start condition of ray
+    r.y = -50
+    r.dy = 1
+    
     c = cylinder.Cylinder()
     
     intersect = c.intersection(r)
